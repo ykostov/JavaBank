bf0624331827cbd460ab9793cdc9e635fcaee140
Yasen Kostov <qskatak04@gmail.com>
2022-07-26 11:33:30 +0300

    add while loop in mainMenu

--- src/Menu.java
+++ src/Menu.java
@@ -4,24 +4,30 @@ import java.util.Scanner;
 
 public class Menu {
     public static void mainMenu() throws IOException {
-        System.out.println(Message.MAINMENU);
-        Scanner scan = new Scanner(System.in);
-        String userInput = scan.nextLine();
-        long moneyInput;
 
 
-        if (userInput.startsWith("new acc"))
-        {
-            Account acc = new Account();
-            acc.setupAccount();
-        }
-        else if (userInput.startsWith("add mo"))
-        {
-            System.out.println(Message.HOWMUCH);
-            Scanner scan1 = new Scanner(System.in);
-            moneyInput = scan1.nextLong();
-            Extranet.setMoneyATM(BigInteger.valueOf(moneyInput));
-            System.out.println(Message.CURRENTMONEY + "" + Extranet.getMoneyATM());
+        while(true) {
+            System.out.println(Message.MAINMENU + " " + Extranet.getMoneyATM());
+
+            Scanner scan = new Scanner(System.in);
+            String userInput = scan.nextLine();
+            long moneyInput;
+
+
+            if (userInput.startsWith("new acc")) {
+                Account acc = new Account();
+                acc.setupAccount();
+            } else if (userInput.startsWith("add mo")) {
+                System.out.println(Message.HOWMUCH);
+                Scanner scan1 = new Scanner(System.in);
+                moneyInput = scan1.nextLong();
+                Extranet.setMoneyATM(BigInteger.valueOf(moneyInput));
+                System.out.println(Message.CURRENTMONEY + "" + Extranet.getMoneyATM());
+            }
+            else if (userInput.startsWith("exit"))
+            {
+                break;
+            }
         }
     }
 }
